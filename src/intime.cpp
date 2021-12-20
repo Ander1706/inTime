46cd6fbd10f9c5c85efd6d9846230bd45f4b4765
Andrey <79419185+Ander1706@users.noreply.github.com>
2021-12-20 14:44:59 +0300

    Initial commit

---
+++ src/intime.cpp
@@ -0,0 +1,40 @@
+/*
+  InTime - Library for asynchronous actions.
+ * Copyright (C) 2018  Andrey Karpushin
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses
+*/
+
+#include "intime.h"
+
+bool InTime::expired(void) {
+    if (!tmr_on)
+        return false;
+    uint32_t ticks = millis();
+    if (time_from <= time_to || ticks < time_from)
+        if (ticks >= time_to) {
+            tmr_on = false;
+            tickCnt = _time;
+            if (_repeat)
+                this->set(_time, _repeat);
+            return true;
+        }
+    if (ticks >= time_from)
+        tickCnt = ticks - time_from;
+    else
+       tickCnt = UINT32_MAX - time_from + ticks;
+    return false;
+}
+
+void InTime::set(uint32_t timeout, bool repeat) {
+    _repeat = repeat;
+    _time = timeout;
+    time_from = millis();
+    time_to = time_from + timeout;
+    tmr_on = true;
+    tickCnt = 0;
+}
---
+++ src/intime.h
@@ -0,0 +1,31 @@
+
+#ifndef INTIME_H
+#define INTIME_H
+
+/*
+  interval - Library for asynchronous actions.
+ * Copyright (C) 2018  Andrey Karpushin
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses
+*/
+
+#include <arduino.h>
+class InTime {
+private:
+  uint32_t _time;
+  bool _repeat;
+
+public:
+  uint32_t time_from;
+  uint32_t time_to;
+  uint32_t tickCnt;
+  bool tmr_on = false;
+  bool expired(void);
+  void set(uint32_t timeout, bool repeat);
+};
+
+#endif
